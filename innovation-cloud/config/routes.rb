2bfa19fa7476a31ea23b2ad673015a162e88a14a
luke <lc169782@protonmail.com>
2022-06-17 10:28:23 +0100

    Created webform for post requests for signing up, added in the resources keyword to routes to map urls to the signups contrlllers based on the 7 standard actions, resource route

--- innovation-cloud/config/routes.rb
+++ innovation-cloud/config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/thanks' => 'pages#thanks'
+
+  
+  resources :signups
+
+  root 'signups#new'
 end
+
